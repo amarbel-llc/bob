1088709080a6be8739f230439f4d8b6d2cc798ed
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2026-04-13 06:34:28 -0400

    feat(batman): add justfile-design-patterns(7) manpage, remove 5 skills
    Convert the design_patterns-just skill into a section-7 manpage built
    with scdoc, updated with patterns audited from across the eng monorepo:
    recipe attributes, variables, parameterized recipes, shebang recipes,
    and cross-justfile delegation.
    
    Remove skills: minus-chorevaults, voldemort, freud, commit,
    design_patterns-just.
    
    Clown :clown: https://github.com/amarbel-llc/clown

--- lib/packages/batman.nix
+++ lib/packages/batman.nix
@@ -105,6 +105,19 @@ let
     '';
   };
 
+  justfile-design-patterns-man = pkgs.stdenvNoCC.mkDerivation {
+    pname = "justfile-design-patterns-man";
+    version = "0.1.0";
+    src = "${src}/doc";
+    nativeBuildInputs = [ pkgs.scdoc ];
+    dontUnpack = true;
+    dontBuild = true;
+    installPhase = ''
+      mkdir -p $out/share/man/man7
+      scdoc < $src/justfile-design-patterns.7.scd > $out/share/man/man7/justfile-design-patterns.7
+    '';
+  };
+
   bats-libs = pkgs.symlinkJoin {
     name = "bats-libs";
     paths = [
@@ -287,6 +300,7 @@ in
       bats-libs
       bats
       robin
+      justfile-design-patterns-man
     ];
   };
   inherit
@@ -299,5 +313,6 @@ in
     bats-libs
     bats
     robin
+    justfile-design-patterns-man
     ;
 }
